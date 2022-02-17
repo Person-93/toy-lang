8f0d8fbb326b7b76152fb02d863ce2296ade98c6
person93 <person93.person93@gmail.com>
2022-02-17 09:22:05 -0500

    refactor(codegen): restructure "Rule" type

--- codegen/src/main.rs
+++ codegen/src/main.rs
@@ -1,30 +1,46 @@
-use anyhow::Result;
-
 use crate::specs::Specs;
+use anyhow::Result;
 use proc_macro2::TokenStream;
 use std::path::Path;
 
 mod collections;
 mod specs;
 
-const RULES: &[Rule] = &[
-  ("tokens", Specs::generate_tokens_mod),
-  ("keywords", Specs::generate_keywords_mod),
-];
-
-type Rule = (&'static str, fn(&Specs) -> Result<TokenStream>);
-
 fn main() -> Result<()> {
+  let rules = [
+    Rule {
+      name: "tokens",
+      func: Specs::generate_tokens_mod,
+    },
+    Rule {
+      name: "keywords",
+      func: Specs::generate_keywords_mod,
+    },
+  ];
+
   let specs = std::fs::read_to_string("specs/specs.toml")?;
   let specs: specs::Specs = toml::de::from_str(&specs)?;
 
-  for (name, func) in RULES {
+  rules.into_iter().try_for_each(|rule| rule.run(&specs))?;
+
+  Ok(())
+}
+
+struct Rule<'r, 's> {
+  name: &'static str,
+  func: fn(&'r Specs<'s>) -> Result<TokenStream>,
+}
+
+impl<'r, 's> Rule<'r, 's> {
+  fn run(self, specs: &'r Specs<'s>) -> Result<()> {
+    let Self { name, func } = self;
+
     let name = Path::new("./crates/compiler/src/")
       .join(name)
       .join("generated.rs");
 
-    std::fs::write(name, func(&specs)?.to_string())?;
-  }
+    std::fs::write(name, func(specs)?.to_string())?;
 
-  Ok(())
+    Ok(())
+  }
 }
