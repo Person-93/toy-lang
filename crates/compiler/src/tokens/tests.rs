4ef68c4d1805020bf7616547a62cbbcb8b7df1d5
person93 <person93.person93@gmail.com>
2022-02-27 11:44:41 -0500

    refactor: get snapshot tests using read_dir instead of glob macro

--- crates/compiler/src/tokens/tests.rs
+++ crates/compiler/src/tokens/tests.rs
@@ -1,28 +1,50 @@
 use super::*;
-use std::fmt::Formatter;
-
-use insta::{assert_snapshot, glob};
+use anyhow::{Context, Result};
+use insta::{assert_snapshot, with_settings};
 use logos::{Logos, Span};
-use std::fs;
+use std::{
+  fmt::{self, Display, Formatter},
+  fs,
+};
 
 #[test]
-fn snapshot() {
-  glob!("toys/*.toy", |file| {
-    let text = fs::read_to_string(file)
-      .unwrap()
+fn snapshot() -> Result<()> {
+  let dir = concat!(env!("CARGO_MANIFEST_DIR"), "/../../toys");
+  for entry in fs::read_dir(dir).with_context(|| format!("missing toys directory: {dir}"))? {
+    let entry = entry.context("failed reading directory entry")?;
+    if !entry
+      .file_type()
+      .context("failed reading file type")?
+      .is_file()
+    {
+      continue;
+    }
+    let path = entry.path();
+    if let Some(ext) = path.extension() {
+      if ext.to_str().unwrap() != "toy" {
+        continue;
+      }
+    }
+
+    let text = fs::read_to_string(&path)
+      .context("failed reading file")?
       // strip '\r' so snapshots match on windows
       .replace('\r', "");
-    assert_snapshot!(Token::lexer(&text)
-      .spanned()
-      .map(|(token, span)| Output(token, span.clone(), &text[span]).to_string())
-      .collect::<Vec<_>>()
-      .join("\n"))
-  });
+    with_settings!({snapshot_suffix => path.file_name().unwrap().to_str().unwrap()}, {
+      assert_snapshot!(Token::lexer(&text)
+        .spanned()
+        .map(|(token, span)| Output(token, span.clone(), &text[span]).to_string())
+        .collect::<Vec<_>>()
+        .join("\n"))
+    });
+  }
+
+  return Ok(());
 
   struct Output<'a>(Token, Span, &'a str);
 
-  impl std::fmt::Display for Output<'_> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+  impl Display for Output<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
       match &self.0 {
         Token::Error => write!(f, "INVALID @ {:?}: {}", self.1, self.2),
         Token::StrLit(lit) => write!(f, "StrLit @ {:?}: {lit}", self.1),
