9bcead3bbdeb08b7b221e9ae79371bead8096bf7
person93 <person93.person93@gmail.com>
2022-03-03 08:51:28 -0500

    feat(ast): add fn parse_single_file

--- compiler/toyc-ast/src/lib.rs
+++ compiler/toyc-ast/src/lib.rs
@@ -2,3 +2,21 @@ pub mod ast;
 pub mod error;
 pub mod tokens;
 pub mod visitor;
+
+pub fn parse_single_file(
+  text: &str,
+  handler: &toyc_errors::Handler,
+) -> Option<crate::ast::File> {
+  use self::{ast::parse, tokens::TokenIter};
+  use chumsky::Parser;
+
+  let tokens = TokenIter::new(text);
+  let (file, errors) = parse::file().parse_recovery(tokens.into_stream());
+  for error in errors {
+    handler
+      .error("parsing failed")
+      .add_span(error.span(), error.to_string())
+      .emit();
+  }
+  file
+}
