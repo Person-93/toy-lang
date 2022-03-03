9ef2a533c2d726f0a8685edd40c4b3ca27b86962
person93 <person93.person93@gmail.com>
2022-03-03 08:49:00 -0500

    feat: add methods to the Handler
    - err_exit: for printing a summary of errors and exiting
    - note: for printing a diagnostic at level Note

--- compiler/toyc-errors/src/handler.rs
+++ compiler/toyc-errors/src/handler.rs
@@ -1,25 +1,98 @@
 use crate::{emitter::Emitter, Diagnostic, DiagnosticBuilder, Level};
-use std::cell::RefCell;
+use std::{cell::RefCell, num::NonZeroI32};
 
 pub struct Handler {
-  emitter: Box<RefCell<dyn Emitter>>,
+  inner: RefCell<HandlerInner>,
+}
+
+struct HandlerInner {
+  emitter: Box<dyn Emitter>,
+  err_count: usize,
+  warn_count: usize,
 }
 
 impl Handler {
-  pub fn emit_diagnostic(&self, diagnostic: &Diagnostic) {
-    // TODO avoid emitting duplicate diagnostics
-    self.emitter.borrow_mut().emit_diagnostic(diagnostic)
+  pub fn new(emitter: Box<dyn Emitter>) -> Handler {
+    Self {
+      inner: RefCell::new(HandlerInner {
+        emitter,
+        err_count: 0,
+        warn_count: 0,
+      }),
+    }
   }
 
-  pub fn fatal(&self, message: impl Into<String>) -> DiagnosticBuilder {
-    DiagnosticBuilder::new(self, Level::Fatal, message)
+  pub fn emit_diagnostic(&self, diagnostic: &Diagnostic) {
+    // TODO avoid emitting duplicate diagnostics
+    let mut inner = self.inner.borrow_mut();
+    match diagnostic.level {
+      Level::Note => {}
+      Level::Warning => inner.warn_count += 1,
+      Level::Error | Level::Fatal => inner.err_count += 1,
+      Level::Bug => todo!(),
+    }
+    inner.emitter.emit_diagnostic(diagnostic);
+  }
+
+  pub fn note(&self, message: impl Into<String>) -> DiagnosticBuilder {
+    DiagnosticBuilder::new(self, Level::Note, message)
+  }
+
+  pub fn warn(&self, message: impl Into<String>) -> DiagnosticBuilder {
+    DiagnosticBuilder::new(self, Level::Warning, message)
   }
 
   pub fn error(&self, message: impl Into<String>) -> DiagnosticBuilder {
     DiagnosticBuilder::new(self, Level::Error, message)
   }
 
-  pub fn warn(&self, message: impl Into<String>) -> DiagnosticBuilder {
-    DiagnosticBuilder::new(self, Level::Warning, message)
+  pub fn fatal(&self, message: impl Into<String>) -> DiagnosticBuilder {
+    DiagnosticBuilder::new(self, Level::Fatal, message)
+  }
+
+  pub fn bug(&self, message: &str) -> ! {
+    self.emit_diagnostic(&Diagnostic::new(Level::Bug, message));
+    panic!();
+  }
+
+  pub fn err_exit(&self, code: NonZeroI32) -> ! {
+    if self.inner.borrow().err_count == 0 {
+      self.bug("attempted to exit with errors but no errors were reported");
+    }
+    self.print_error_report();
+    std::process::exit(code.get());
+  }
+
+  fn print_error_report(&self) {
+    let mut inner = self.inner.borrow_mut();
+
+    let warn_count = inner.warn_count;
+    let err_count = inner.err_count;
+
+    let warnings = || match warn_count {
+      0 => String::new(),
+      1 => "1 warning emitted".to_string(),
+      count => format!("{count} warnings emitted"),
+    };
+
+    let errors = || match err_count {
+      0 => String::new(),
+      1 => "aborting due to previous error".to_string(),
+      count => format!("aborting due to {count} previous errors"),
+    };
+
+    match (inner.warn_count == 0, inner.err_count == 0) {
+      (true, true) => (),
+      (true, false) => inner
+        .emitter
+        .emit_diagnostic(&Diagnostic::new(Level::Warning, warnings())),
+      (false, true) => inner
+        .emitter
+        .emit_diagnostic(&Diagnostic::new(Level::Fatal, errors())),
+      (false, false) => inner.emitter.emit_diagnostic(&Diagnostic::new(
+        Level::Fatal,
+        format!("{}; {}", errors(), warnings()),
+      )),
+    }
   }
 }
