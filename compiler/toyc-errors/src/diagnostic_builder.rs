4eef6012a45d092bf934aac6826b296f184e74a4
person93 <person93.person93@gmail.com>
2022-03-03 05:24:10 -0500

    feat(errors): forward messages from DiagnosticBuilder to Diagnostic

--- compiler/toyc-errors/src/diagnostic_builder.rs
+++ compiler/toyc-errors/src/diagnostic_builder.rs
@@ -3,7 +3,9 @@ use std::{
   ops::{Deref, DerefMut},
   thread::panicking,
 };
+use toyc_span::Span;
 
+#[must_use]
 pub struct DiagnosticBuilder<'a> {
   diagnostic: Diagnostic,
   state: DiagnosticBuilderState<'a>,
@@ -12,6 +14,25 @@ pub struct DiagnosticBuilder<'a> {
 enum DiagnosticBuilderState<'a> {
   Emittable(&'a Handler),
   Handled,
+}
+
+macro_rules! forward {
+  (
+    pub fn $n:ident(&self, $($name:ident: $ty:ty),* $(,)?) -> &Self
+  ) => {
+    pub fn $n(&self, $($name: $ty),*) -> &Self {
+      self.diagnostic.$n($(name),*);
+      self
+    }
+  };
+  (
+    pub fn $n:ident(&mut self, $($name:ident: $ty:ty),* $(,)?) -> &mut Self
+  ) => {
+    pub fn $n(&mut self, $($name: $ty),*) -> &mut Self {
+        self.diagnostic.$n($($name),*);
+        self
+      }
+  }
 }
 
 impl<'a> DiagnosticBuilder<'a> {
@@ -26,7 +47,7 @@ impl<'a> DiagnosticBuilder<'a> {
     }
   }
 
-  pub fn emit(mut self) {
+  pub fn emit(&mut self) {
     match self.state {
       DiagnosticBuilderState::Emittable(handler) => {
         handler.emit_diagnostic(&self.diagnostic);
@@ -39,6 +60,14 @@ impl<'a> DiagnosticBuilder<'a> {
   pub fn cancel(mut self) {
     self.state = DiagnosticBuilderState::Handled;
   }
+
+  forward!(
+    pub fn add_span(&mut self, span: Span, label: impl Into<String>) -> &mut Self
+  );
+
+  forward!(
+    pub fn attach_note(&mut self, label: impl Into<String>) -> &mut Self
+  );
 }
 
 impl Deref for DiagnosticBuilder<'_> {
