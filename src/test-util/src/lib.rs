583cf442de6a35d5577555bc9789a3981dede382
person93 <person93.person93@gmail.com>
2022-03-06 12:02:52 -0500

    test: add struct "TestSession"

--- src/test-util/src/lib.rs
+++ src/test-util/src/lib.rs
@@ -1,5 +1,10 @@
+use core::ops::{Deref, DerefMut};
 pub use insta::{assert_debug_snapshot, assert_snapshot, glob, with_settings};
-use std::fs;
+use parking_lot::{RwLock, RwLockReadGuard};
+use std::{fs, rc::Rc};
+use toyc_errors::emitter::Emitter;
+use toyc_errors::{Diagnostic, Handler};
+use toyc_session::Session;
 
 pub fn snapshots(f: fn(&str) -> ()) {
   glob!("toys/*.toy", |path| {
@@ -14,4 +19,52 @@ pub fn snapshots(f: fn(&str) -> ()) {
       f(&text);
     });
   });
+}
+
+pub struct TestSession {
+  inner: Session,
+  diagnostics: Rc<RwLock<Vec<Diagnostic>>>,
+}
+
+impl TestSession {
+  pub fn new<T: ToString>(job_name: T) -> TestSession {
+    let diagnostics = Rc::new(RwLock::default());
+    TestSession {
+      inner: Session::new(
+        job_name.to_string(),
+        None,
+        Handler::new(Box::new(TestEmitter(diagnostics.clone()))),
+      ),
+      diagnostics,
+    }
+  }
+
+  pub fn diagnostics(
+    this: &TestSession,
+  ) -> impl Deref<Target = [Diagnostic]> + '_ {
+    RwLockReadGuard::map(this.diagnostics.read(), Vec::as_slice)
+  }
+}
+
+impl Deref for TestSession {
+  type Target = Session;
+
+  fn deref(&self) -> &Self::Target {
+    &self.inner
+  }
+}
+
+impl DerefMut for TestSession {
+  fn deref_mut(&mut self) -> &mut Self::Target {
+    &mut self.inner
+  }
+}
+
+#[derive(Debug)]
+struct TestEmitter(Rc<RwLock<Vec<Diagnostic>>>);
+
+impl Emitter for TestEmitter {
+  fn emit_diagnostic(&mut self, diagnostic: &Diagnostic) {
+    self.0.write().push(diagnostic.clone());
+  }
 }
